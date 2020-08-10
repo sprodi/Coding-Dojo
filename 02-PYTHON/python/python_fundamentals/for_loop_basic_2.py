42c7a44cfd17d403a01f3325c9ab4e1c184774da
Sprodi <seanprodi@gmail.com>
2020-08-10 11:56:27 -0700

    updated for_loop_basic_2.py

--- 02-PYTHON/python/python_fundamentals/for_loop_basic_2.py
+++ 02-PYTHON/python/python_fundamentals/for_loop_basic_2.py
@@ -1,82 +1,80 @@
 # 1 Biggie Size - Given a list, write a function that changes all positive numbers in the list to "big".
-def biggie_size(lst):
-   for i in range(0, len(lst)):
-      if lst[i] > 0:
-         lst[i] = 'big'
-   return lst
-# biggie_size([-1,3,5,-5])
+def biggie_size(list):
+   for i in range(0, len(list)):
+      if list[i] > 0:
+         list[i] = 'big'
+   return list
+# print(biggie_size([-1,3,5,-5]))
 
 # 2 Count Positives - Given a list of numbers, create a function to replace the last value with the number of positive values. (Note that zero is not considered to be a positive number).
-def count_positives(lst):
+def count_positives(list):
    count = 0
-   for i in range(0, len(lst)):
-      if lst[i] > 0:
+   for i in range(0, len(list)):
+      if list[i] > 0:
          count += 1
-   lst[-1] = count
-   return lst
-# count_positives([-1,1,1,1])
-# count_positives([1,6,-4,-2,-7,-2])
+   list[-1] = count
+   return list
+# print(count_positives([-1,1,1,1]))
+# print(count_positives([1,6,-4,-2,-7,-2]))
 
 # 3 Sum Total - Create a function that takes a list and returns the sum of all the values in the array.
-def sum_total(lst):
+def sum_total(list):
    sumTotal = 0
-   for i in range(0, len(lst)):
-      sumTotal += lst[i]
+   for i in range(0, len(list)):
+      sumTotal += list[i]
    return sumTotal
-# sum_total([1,2,3,4])
-# sum_total([6,3,-2])
+# print(sum_total([1,2,3,4]))
+# print(sum_total([6,3,-2]))
 
 # 4 Average - Create a function that takes a list and returns the average of all the values.
-def average(lst):
+def average(list):
    sumTotal = 0
-   for i in range(0, len(lst)):
-      sumTotal += lst[i]
-   return sumTotal / len(lst)
-# average([1,2,3,4])
+   for i in range(0, len(list)):
+      sumTotal += list[i]
+   return sumTotal / len(list)
+# print(average([1,2,3,4]))
 
 # 5 Length - Create a function that takes a list and returns the length of the list.
-def length(lst):
-  length = 0
-  for i in range(0, len(lst)):
-    if i < len(lst):
-      length += 1
-  return length
-# length([37,2,1,-9])
-# length([])
+def length(list):
+  return len(list)
+# print(length([37,2,1,-9]))
+# print(length([]))
 
 # 6 Minimum - Create a function that takes a list of numbers and returns the minimum value in the list. If the list is empty, have the function return False.
-def minimum(lst):
-   minVal = lst[0]
-   if len(lst) == 0:
+def minimum(list):
+   if len(list) == 0:
       return False
-   for i in lst:
-      if i < minVal:
-         minVal = i
-   return minVal
-# minimum([37,2,1,-9])
-# minimum([2,-5,6,2])
-# minimum([])
+   else:
+      minVal = list[0]
+      for i in range(len(list)):
+         if list[i] < minVal:
+            minVal = list[i]
+      return minVal
+# print(minimum([37,2,1,-9]))
+# print(minimum([2,-5,6,2]))
+# print(minimum([]))
 
 # 7 Maximum - Create a function that takes a list and returns the maximum value in the array. If the list is empty, have the function return False.
-def maximum(lst):
-   maxVal = lst[0]
-   if len(lst) == 0:
+def maximum(list):
+   if len(list) == 0:
       return False
-   for i in lst:
-      if i > maxVal:
-         maxVal = i
-   return maxVal
-# maximum([37,2,1,-9])
-# maximum([2,-5,6,2])
-# maximum([])
+   else:
+      maxVal = list[0]
+      for i in list:
+         if i > maxVal:
+            maxVal = i
+      return maxVal
+# print(maximum([37,2,1,-9]))
+# print(maximum([2,-5,6,2]))
+# print(maximum([]))
 
 # 8 Ultimate Analysis - Create a function that takes a list and returns a dictionary that has the sumTotal, average, minimum, maximum and length of the list.
-def ultimate_analysis(lst):
-   sum = sum_total(lst)
-   avg = average(lst)
-   min = minimum(lst)
-   max = maximum(lst)
-   len = length(lst)
+def ultimate_analysis(list):
+   sum = sum_total(list)
+   avg = average(list)
+   min = minimum(list)
+   max = maximum(list)
+   len = length(list)
    dict = {
       'sumTotal': sum,
       'average': avg,
@@ -84,13 +82,13 @@ def ultimate_analysis(lst):
       'maximum': max,
       'length': len
    }
-   print(dict)
-# ultimate_analysis([37,2,1,-9])
+   return dict
+print(ultimate_analysis([37,2,1,-9]))
 
 # 9 Reverse List - Create a function that takes a list and return that list with values reversed. Do this without creating a second list. (This challenge is known to appear during basic technical interviews.)
-def reverse_list(lst):
+def reverse_list(list):
    newList = []
-   for i in range(1, len(lst)+1):
-     newList.append(lst[-i])
+   for i in range(1, len(list)+1):
+     newList.append(list[-i])
    return newList
-# reverse_list([37,2,1,-9])
+# print(reverse_list([37,2,1,-9]))
