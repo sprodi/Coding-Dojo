3ebfe2d36e8188103ad2b4d5e3a0afbc62ab7cfb
Sprodi <seanprodi@gmail.com>
2020-08-18 13:44:35 -0700

    added chaining_methods.py, updated users.py

--- 02-PYTHON/python/OOP/chaining_methods.py
+++ 02-PYTHON/python/OOP/chaining_methods.py
@@ -7,6 +7,7 @@ class User():
    def make_deposit(self, amount):
       self.balance += amount
       print(f"{self.username} Deposited ${amount}")
+      return self
 
    def make_withdrawal(self, amount):
       if self.balance < amount:
@@ -14,6 +15,7 @@ class User():
       else:
          self.balance -= amount
          print(f"{self.username} Withdrew ${amount}")
+      return self
 
 
    def transfer_money(self, other_user, amount):
@@ -23,12 +25,13 @@ class User():
          self.balance -= amount
          other_user.balance += amount
          print(f"{self.username} Transfered ${amount} to {other_user.username}")
-
+      return self
 
 
    def display_user_balance(self):
-         return f"User: {self.username}, Balance: ${self.balance}"
-
+      print(f"User: {self.username}, Balance: ${self.balance}")
+      return self 
+      
 
 print()      
 x = User("Sprodi", 0)
@@ -36,30 +39,17 @@ y = User("BentoBox", 0)
 z = User("Parker", 0)
 
 # Sprodi
-x.make_deposit(500)
-x.make_deposit(200)
-x.make_deposit(50)
-x.make_withdrawal(150)
-print(x.display_user_balance())
+x.make_deposit(500).make_deposit(200).make_deposit(50).make_withdrawal(150).display_user_balance()
 print()
 
 # BentoBox
-y.make_deposit(1200)
-y.make_deposit(200)
-y.make_withdrawal(100)
-y.make_withdrawal(450)
-print(y.display_user_balance())
+y.make_deposit(1200).make_deposit(200).make_withdrawal(100).make_withdrawal(450).display_user_balance()
 print()
 
 # Parker
-z.make_deposit(2500)
-z.make_withdrawal(300)
-z.make_withdrawal(100)
-z.make_withdrawal(100)
-print(z.display_user_balance())
+z.make_deposit(2500).make_withdrawal(300).make_withdrawal(100).make_withdrawal(100).display_user_balance()
 print()
 
 # Transfer
-x.transfer_money(z, 600)
-print(x.display_user_balance())
-print(z.display_user_balance())
+x.transfer_money(z, 600).display_user_balance()
+z.display_user_balance()
